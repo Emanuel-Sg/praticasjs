d271c009d704a6ca922f5124569ad1a0f2981ab2
Emanuel Bragança <emgbraganca@gmail.com>
2021-11-28 15:55:34 -0300

    alteracao

--- ex3/main.js
+++ ex3/main.js
@@ -1,7 +1,8 @@
 const mudaCor = document.getElementById('mudaCor');
 
 function update() {
-    document.querySelector('p').style.backgroundColor = "blue";
+    document.querySelector('p').style.color = "grey";
+    document.querySelector('p').style.fontFamily = "arial";
+    document.querySelector('p').style.fontSizeAdjust = "2";
 }
-
 
